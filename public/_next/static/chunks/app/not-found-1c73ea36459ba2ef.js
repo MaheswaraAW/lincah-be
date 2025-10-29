64f769d8e45bc2b8dbd5ed5ed3eceeb9cf22efad
MaheswaraAW <maheswara.a.w@gmail.com>
2025-10-29 12:30:40 +0700

    perbaiki frontend

---
+++ public/_next/static/chunks/app/not-found-1c73ea36459ba2ef.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9160,3134,9236,1327,9658,5849,5599],{12088:function(e,n,s){Promise.resolve().then(s.bind(s,49147)),Promise.resolve().then(s.t.bind(s,72972,23))}},function(e){e.O(0,[5338,9147,2971,2117,1744],function(){return e(e.s=12088)}),_N_E=e.O()}]);
