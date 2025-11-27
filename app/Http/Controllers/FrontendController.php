0eb32921070e96d15b85a82e036815266a87fb4a
dinkes <dinkes@semarangkota.go.id>
2025-11-27 08:15:58 +0000

    fix front end

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -27,6 +27,14 @@ class FrontendController extends Controller
     // dd("tes");
     // return view('front_end.index.html');
     return view()->file(resource_path('views/front_end/forum.html'));
+    // return view()->file(public_path('front_end/forum.html'));
+    // $path = public_path('front_end/forum.html');
+
+    // abort_unless(File::exists($path), 404, 'File forum.html tidak ditemukan di public/front_end');
+
+    // return response()->file($path, [
+    //     'Content-Type' => 'text/html; charset=UTF-8',
+    // ]);
   }
 
   public function publikasi()
